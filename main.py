547c8752ef37d96d2a05ef73f91de1e3c5392b7a
prieta1975 <antonio_prieto_jimenez@hotmail.com>
2015-12-12 23:34:43 +0100

    Prueba desde pycharm

---
+++ main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import filedialog
+
+fileName = "C:"
+
+def doNothing():
+    print("Nada")
+
+def openFile():
+    f = filedialog.askopenfile(parent=root)
+    print(f.name)
+
+
+# Window creation
+root = Tk()
+
+# Creates menu bar & config menu bar in window object
+menuBar = Menu(master=root)
+root.config(menu=menuBar)
+
+# Creates File menu option & adds options for File menu option
+fileMenu = Menu(master=menuBar)
+menuBar.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="New Project...", command=openFile)
+
+# creates sub menu "SubMenu" for File menu option "Submenu" & creates options for sub menu
+subMenu = Menu(master=fileMenu)
+fileMenu.add_cascade(label="Submenu", menu=subMenu)
+subMenu.add_command(label="Opc Subm")
+
+#Creates File menu additional options
+fileMenu.add_command(label="New...", command=doNothing)
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit", command=doNothing)
+
+# Creates Edit menu option
+editMenu = Menu(master=menuBar)
+menuBar.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Copy", command=doNothing)
+
+# Keeps window open
+root.mainloop()
